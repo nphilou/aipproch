4b7dab5e24ed9545be33cd8eca5a64309c3c3cf1
Philippe <nphilougmail.com>
2017-10-03 17:40:09 +0200

    code cleaning

--- src/search/Problem.java
+++ src/search/Problem.java
@@ -4,106 +4,93 @@ import java.util.*;
 import java.util.function.Function;
 
 
-public class Problem<State extends Searchable<State,Action>,Action> {
-	
-	State initialState;
-	State finalState;
+public class Problem<State extends Searchable<State, Action>, Action> {
 
-	public boolean goal_test(State s){
-		return finalState.equals(s);
-	}
-	
-	public Problem(State start, State stop){
-		initialState = start;
-		finalState = stop;
-	}
-	
-	public String toString() {
-		return initialState + "\n" + " V \n\n" + finalState;	
-	}
+    State initialState;
+    State finalState;
 
-	private void breadth(ArrayList<State> states, State finalState, Set<State> visited, int i){
-		ArrayList<State> newStates = new ArrayList<>();
-		for (State currentState: states) {
-			if (goal_test(currentState)){
-				//System.out.println("currentState = \n" + currentState);
-				//System.out.println("currentState == finalState :D");
-				return;
-			}
-			for (Action action: currentState.getActions()) {
-				System.out.println("new : \n" + currentState.execute(action));
-				if (!visited.contains(currentState.execute(action))) {
-					//System.out.println("visited don't contain nextState");
-					//System.out.println("-------------------------------");
-					newStates.add(currentState.execute(action));
-					if (goal_test(currentState.execute(action))){
-						return;
-					}
-				}
-
-			}
-			visited.addAll(newStates);
-
-		}
-		i++;
-		System.out.println("i = " + i);
-		breadth(newStates, finalState, visited, i);
-	}
-
-	private void depth(ArrayList<State> states, State finalState, Set<State> visited, int i){
-		System.out.println("i = " + i);
-		ArrayList<State> newStates = new ArrayList<>();
-		for (State currentState: states) {
-			if (goal_test(currentState)){
-				return;
-			}
-			for (Action action: currentState.getActions()) {
-				System.out.println("new : \n" + currentState.execute(action));
-				if (!visited.contains(currentState.execute(action))) {
-					System.out.println("visited don't contain nextState");
-					System.out.println("-------------------------------");
-					newStates.add(currentState.execute(action));
-					visited.addAll(newStates);
-					if (goal_test(currentState.execute(action))){
-						return;
-					}
-					depth(newStates, finalState, visited, ++i);
-				}
-				if (visited.contains(finalState)){
-					return;
-				}
-			}
-		}
-	}
-	
-	public int bfs(){
-		Set<State> visited = new HashSet<>();
-		visited.add(initialState);
-
-		ArrayList<State> initial = new ArrayList<>();
-		initial.add(initialState);
-		System.out.println("initialState = " + initialState);
-		breadth(initial, finalState, visited, 0);
-
-		return visited.size();
+    public boolean goal_test(State s) {
+        return finalState.equals(s);
     }
-	
-	
-	public int dfs(){
-		Set<State> visited = new HashSet<>();
-		visited.add(initialState);
 
-		ArrayList<State> initial = new ArrayList<>();
-		initial.add(initialState);
-		System.out.println("initialState = " + initialState);
-		System.out.println("visited = " + visited);
-		depth(initial, finalState, visited, 0);
-
-		return visited.size();
+    public Problem(State start, State stop) {
+        initialState = start;
+        finalState = stop;
     }
-	
-	public int aStar(Function<State,Double> g, Function<State,Double> h ){
-		// to be completed
+
+    public String toString() {
+        return initialState + "\n" + " V \n\n" + finalState;
+    }
+
+    private void breadth(ArrayList<State> states, State finalState, Set<State> visited, int i) {
+        ArrayList<State> newStates = new ArrayList<>();
+        for (State currentState : states) {
+            if (goal_test(currentState)) {
+                return;
+            }
+            for (Action action : currentState.getActions()) {
+                System.out.println("new : \n" + currentState.execute(action));
+                if (!visited.contains(currentState.execute(action))) {
+                    //System.out.println("visited don't contain nextState");
+                    //System.out.println("-------------------------------");
+                    newStates.add(currentState.execute(action));
+                    if (goal_test(currentState.execute(action))) {
+                        return;
+                    }
+                }
+            }
+            visited.addAll(newStates);
+        }
+        breadth(newStates, finalState, visited, ++i);
+    }
+
+    private void depth(ArrayList<State> states, State finalState, Set<State> visited, int i) {
+        ArrayList<State> newStates = new ArrayList<>();
+        for (State currentState : states) {
+            if (goal_test(currentState)) {
+                return;
+            }
+            for (Action action : currentState.getActions()) {
+                if (!visited.contains(currentState.execute(action))) {
+                    newStates.add(currentState.execute(action));
+                    visited.addAll(newStates);
+                    if (goal_test(currentState.execute(action))) {
+                        return;
+                    }
+                    depth(newStates, finalState, visited, ++i);
+                }
+                if (visited.contains(finalState)) {
+                    return;
+                }
+            }
+        }
+    }
+
+    public int bfs() {
+        Set<State> visited = new HashSet<>();
+        visited.add(initialState);
+
+        ArrayList<State> initial = new ArrayList<>();
+        initial.add(initialState);
+        breadth(initial, finalState, visited, 0);
+
+        return visited.size();
+    }
+
+
+    public int dfs() {
+        Set<State> visited = new HashSet<>();
+        visited.add(initialState);
+
+        ArrayList<State> initial = new ArrayList<>();
+        initial.add(initialState);
+        depth(initial, finalState, visited, 0);
+
+        return visited.size();
+    }
+
+    public int aStar(Function<State, Double> g, Function<State, Double> h) {
+        // Hell no
         return 0;
     }
 
--- src/search/Puzzle.java
+++ src/search/Puzzle.java
@@ -1,4 +1,5 @@
 package search;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -11,199 +12,214 @@ import java.util.Map;
 
 
 public class Puzzle implements Searchable<Puzzle, PuzzleAction>, Cloneable {
-	
-	/** String representation of a puzzle: the string contains 9 digits and represent the 
-	 * matrix entered row by row. The digit 0 represents the empty cell.
-	 */
-	private String puzzle;
-	
-	/**
-	 * Default constructor, the game is empty
-	 */
-	Puzzle(){
-		puzzle = "";
-	}
-	
-	/**
-	 * Builds an instance of the 8 puzzle, the initial state is represented by the string passed in parameter
-	 * @param s the string representation of the initial state
-	 */
-	Puzzle(String s){
-		puzzle = "";		
-		if (s.length()!=9){
-			System.out.println("la chaine n'est pas de la bonne longueur" + s.length());
-		}
-		puzzle = s;
-	}
-	
-	/**
-	 * returns a String representation of the state
-	 *  @return a string representation of the state (3x3 matrix)
-	 */
-	@Override public String toString(){
-		String result="";
-		for (int i=0;i<9;i++){
-			if (puzzle.charAt(i)=='0')
-				result += ". ";
-			else	
-				result += puzzle.charAt(i)+" ";
-			if (i==2 || i==5 || i==8)
-				result += "\n";
-		}
-		result+= gValue + "+" + hValue +"="+(gValue+hValue)+ "[" + puzzle +"]\n";
-		return result;
-	}
-	
-	@Override public int hashCode() {
-		return puzzle.hashCode();
-	}
-	
-	/**
-	 * clone method
-	 * @return clone of the 8puzzle
-	 */
-	@Override public Puzzle clone(){
-		return new Puzzle(puzzle);
-	}
-	
-	/**
-	 * test the equality between two states
-	 * @return true if the two states are equal, false otherwise
-	 */
-	@Override public boolean equals(Object p){
-		if (p instanceof Puzzle){
-			Puzzle q = (Puzzle) p;
-			return puzzle.equals(q.puzzle);
-		}
-		else
-			return false;
-	}
-	
-	
-		public String getDesc() {return puzzle;}
-	/**
-	 * this method returns the possible actions that can be taken from that state
-	 * @return a list of action that are available from that state
-	 */
-	public List<PuzzleAction> getActions(){
-		List<PuzzleAction> l = new ArrayList<PuzzleAction>();
-		int vide = puzzle.indexOf("0");
-		if (vide<3)// ligne du haut
-			l.add(PuzzleAction.up);
-		if (2<vide && vide<6){ // ligne du milieu
-			l.add(PuzzleAction.up); l.add(PuzzleAction.down);
-		}	
-		if (vide>5)// ligne du bas
-			l.add(PuzzleAction.down);
-		if (vide%3==0)//colonne de gauche
-			l.add(PuzzleAction.left); 
-		if ((vide-1)%3==0){ // colonne du milieu
-			l.add(PuzzleAction.left); l.add(PuzzleAction.right);
-			}
-		if ((vide+1)%3==0) // colonne de droite
-				l.add(PuzzleAction.right);
-		return l;
-	}
-	
-	/**
-	 * return the state that results from taking action <code>a</code> in the current state
-	 * @param a action to be taken (we assume it is a valid action)
-	 * @return a new puzzle that is the result of taking action <code>a</code> in the current state
-	 */
-	public Puzzle execute(PuzzleAction a){
-		Puzzle q = new Puzzle();
-		int vide = puzzle.indexOf("0");
-		switch (a){
-			case up:
-				q.puzzle = 	puzzle.substring(0, vide) 
-					+ puzzle.substring(vide+3, vide+4) 
-					+ puzzle.substring(vide+1, vide+3)
-					+ "0"
-					+ puzzle.substring(vide+4, 9); 
-				break;
-			case down:
-				q.puzzle = puzzle.substring(0, vide-3) 
-					+ "0"			
-					+ puzzle.substring(vide-2, vide) 
-					+ puzzle.substring(vide-3, vide-2)
-					+ puzzle.substring(vide+1, 9);
-				break;
-			case left:
-				q.puzzle = puzzle.substring(0, vide)
-				 + puzzle.substring(vide+1, vide+2) 
-				 + "0"
-				 + puzzle.substring(vide+2, 9);
-				break;
-			case right:
-				q.puzzle = puzzle.substring(0, vide-1)
-						 + "0"
-						 + puzzle.substring(vide-1, vide)
-						 + puzzle.substring(vide+1, 9); 
-				break;
-		}
-		return q;
-	}
-	
-	
-	
-	public int getHeuristic(){
-		return hValue;
-	}
-	
-	/** predecessor in the path from the initial state to a final state */
-	private Puzzle predecessor;
-	
-	/** heuristic values */
-	private int gValue=0;
-	/** heuristic values */
-	private int hValue=0;
-	
-	/**
-	 * 
-	 */
-	public void setPredecessor(Puzzle p){
-		predecessor = p;
-	}
-	
-	public Puzzle getPredecessor(){
-		return predecessor;
-	}
-	
-	public void setValueG(double cost){
-		gValue = (int) cost;
-	}
-	
-	public int getValueG(){
-		return gValue;
-	}
-	
-	
-	
-	public void setValueH(double val) {
-		hValue = (int) val;
-	}
-	
-	@Override
-	public int compareTo(Searchable<Puzzle, PuzzleAction> o) {
-		if (!(o instanceof Puzzle)){
-			System.out.println("Problem comparing, I should be comparing with another puzzle");
-			return -1;
-		}
-		else{
-			Puzzle p = (Puzzle) o;
-			return -(p.getValueG()+p.getHeuristic() - getValueG()-getHeuristic());
-		}
-	}
-	
-	public int depth() {
-		int d=0;
-		Puzzle prev = predecessor;
-		while (prev!=null) {
-			d++;
-			prev = prev.predecessor;
-		}
-		return d;
-	}
-	
-	
+
+    /**
+     * String representation of a puzzle: the string contains 9 digits and represent the
+     * matrix entered row by row. The digit 0 represents the empty cell.
+     */
+    private String puzzle;
+
+    /**
+     * Default constructor, the game is empty
+     */
+    Puzzle() {
+        puzzle = "";
+    }
+
+    /**
+     * Builds an instance of the 8 puzzle, the initial state is represented by the string passed in parameter
+     *
+     * @param s the string representation of the initial state
+     */
+    Puzzle(String s) {
+        puzzle = "";
+        if (s.length() != 9) {
+            System.out.println("la chaine n'est pas de la bonne longueur" + s.length());
+        }
+        puzzle = s;
+    }
+
+    /**
+     * returns a String representation of the state
+     *
+     * @return a string representation of the state (3x3 matrix)
+     */
+    @Override
+    public String toString() {
+        String result = "";
+        for (int i = 0; i < 9; i++) {
+            if (puzzle.charAt(i) == '0')
+                result += ". ";
+            else
+                result += puzzle.charAt(i) + " ";
+            if (i == 2 || i == 5 || i == 8)
+                result += "\n";
+        }
+        result += gValue + "+" + hValue + "=" + (gValue + hValue) + "[" + puzzle + "]\n";
+        return result;
+    }
+
+    @Override
+    public int hashCode() {
+        return puzzle.hashCode();
+    }
+
+    /**
+     * clone method
+     *
+     * @return clone of the 8puzzle
+     */
+    @Override
+    public Puzzle clone() {
+        return new Puzzle(puzzle);
+    }
+
+    /**
+     * test the equality between two states
+     *
+     * @return true if the two states are equal, false otherwise
+     */
+    @Override
+    public boolean equals(Object p) {
+        if (p instanceof Puzzle) {
+            Puzzle q = (Puzzle) p;
+            return puzzle.equals(q.puzzle);
+        } else
+            return false;
+    }
+
+
+    public String getDesc() {
+        return puzzle;
+    }
+
+    /**
+     * this method returns the possible actions that can be taken from that state
+     *
+     * @return a list of action that are available from that state
+     */
+    public List<PuzzleAction> getActions() {
+        List<PuzzleAction> l = new ArrayList<PuzzleAction>();
+        int vide = puzzle.indexOf("0");
+        if (vide < 3)// ligne du haut
+            l.add(PuzzleAction.up);
+        if (2 < vide && vide < 6) { // ligne du milieu
+            l.add(PuzzleAction.up);
+            l.add(PuzzleAction.down);
+        }
+        if (vide > 5)// ligne du bas
+            l.add(PuzzleAction.down);
+        if (vide % 3 == 0)//colonne de gauche
+            l.add(PuzzleAction.left);
+        if ((vide - 1) % 3 == 0) { // colonne du milieu
+            l.add(PuzzleAction.left);
+            l.add(PuzzleAction.right);
+        }
+        if ((vide + 1) % 3 == 0) // colonne de droite
+            l.add(PuzzleAction.right);
+        return l;
+    }
+
+    /**
+     * return the state that results from taking action <code>a</code> in the current state
+     *
+     * @param a action to be taken (we assume it is a valid action)
+     * @return a new puzzle that is the result of taking action <code>a</code> in the current state
+     */
+    public Puzzle execute(PuzzleAction a) {
+        Puzzle q = new Puzzle();
+        int vide = puzzle.indexOf("0");
+        switch (a) {
+            case up:
+                q.puzzle = puzzle.substring(0, vide)
+                        + puzzle.substring(vide + 3, vide + 4)
+                        + puzzle.substring(vide + 1, vide + 3)
+                        + "0"
+                        + puzzle.substring(vide + 4, 9);
+                break;
+            case down:
+                q.puzzle = puzzle.substring(0, vide - 3)
+                        + "0"
+                        + puzzle.substring(vide - 2, vide)
+                        + puzzle.substring(vide - 3, vide - 2)
+                        + puzzle.substring(vide + 1, 9);
+                break;
+            case left:
+                q.puzzle = puzzle.substring(0, vide)
+                        + puzzle.substring(vide + 1, vide + 2)
+                        + "0"
+                        + puzzle.substring(vide + 2, 9);
+                break;
+            case right:
+                q.puzzle = puzzle.substring(0, vide - 1)
+                        + "0"
+                        + puzzle.substring(vide - 1, vide)
+                        + puzzle.substring(vide + 1, 9);
+                break;
+        }
+        return q;
+    }
+
+
+    public int getHeuristic() {
+        return hValue;
+    }
+
+    /**
+     * predecessor in the path from the initial state to a final state
+     */
+    private Puzzle predecessor;
+
+    /**
+     * heuristic values
+     */
+    private int gValue = 0;
+    /**
+     * heuristic values
+     */
+    private int hValue = 0;
+
+    /**
+     *
+     */
+    public void setPredecessor(Puzzle p) {
+        predecessor = p;
+    }
+
+    public Puzzle getPredecessor() {
+        return predecessor;
+    }
+
+    public void setValueG(double cost) {
+        gValue = (int) cost;
+    }
+
+    public int getValueG() {
+        return gValue;
+    }
+
+    public void setValueH(double val) {
+        hValue = (int) val;
+    }
+
+    @Override
+    public int compareTo(Searchable<Puzzle, PuzzleAction> o) {
+        if (!(o instanceof Puzzle)) {
+            System.out.println("Problem comparing, I should be comparing with another puzzle");
+            return -1;
+        } else {
+            Puzzle p = (Puzzle) o;
+            return -(p.getValueG() + p.getHeuristic() - getValueG() - getHeuristic());
+        }
+    }
+
+    public int depth() {
+        int d = 0;
+        Puzzle prev = predecessor;
+        while (prev != null) {
+            d++;
+            prev = prev.predecessor;
+        }
+        return d;
+    }
 }
